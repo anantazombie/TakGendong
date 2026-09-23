cfa8e01ef12ec626758dc2fe99b6b70745e79c89
anantazombie <dhsnrdnn@gmail.com>
2026-09-23 14:55:40 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE="angkut-v1";
+const ASSETS=["index.html","manifest.webmanifest","icon.svg"];
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));
+  self.skipWaiting();
+});
+self.addEventListener("activate",e=>{ self.clients.claim(); });
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r || fetch(e.request).catch(()=>caches.match("index.html")))
+  );
+});
